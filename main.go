ddad5f97494085e0dd9084cebebeca7a0ca9dc2f
Michael-Ralph <MichaelR@praxima.com>
2025-03-26 07:54:09 +0200

    Add user input

--- main.go
+++ main.go
@@ -6,12 +6,25 @@ import (
 )
 
 func main() {
+	// Variables
 	const inflationRate = 2.5
-	investmentAmount, years, expectedReturnRate := 1000.0, 10.0, 5.5
+	var investmentAmount, years, expectedReturnRate float64
 
+	// Variable inputs
+	fmt.Printf("How much do you want to invest: ")
+	fmt.Scan(&investmentAmount)
+	fmt.Printf("Over how many years do you want to invest: ")
+	fmt.Scan(&years)
+	fmt.Printf("What is your expected return rate: ")
+	fmt.Scan(&expectedReturnRate)
+
+	// Calculations
 	futureValue := investmentAmount * math.Pow(1+expectedReturnRate/100, years)
 	futureRealValue := futureValue / math.Pow(1+inflationRate/100, years)
 
+	// Outputs
+	fmt.Print("Future value of investment: ")
 	fmt.Println(futureValue)
+	fmt.Print("Future value of investment adjusted for inflation: ")
 	fmt.Println(futureRealValue)
 }
