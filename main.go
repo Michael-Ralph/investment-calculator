99adcf21b65197fc027ea5a6e403147f17e81c09
Michael-Ralph <MichaelR@praxima.com>
2025-03-31 11:52:18 +0200

    Add functions

--- main.go
+++ main.go
@@ -5,26 +5,37 @@ import (
 	"math"
 )
 
-func main() {
-	// Variables
-	const inflationRate = 2.5
-	var investmentAmount, years, expectedReturnRate float64
+const inflationRate = 2.5
 
+func main() {
 	// Variable inputs
-	fmt.Print("How much do you want to invest: R")
-	fmt.Scan(&investmentAmount)
-	fmt.Print("Over how many years do you want to invest: ")
-	fmt.Scan(&years)
-	fmt.Print("What is your expected return rate: ")
-	fmt.Scan(&expectedReturnRate)
+	investmentAmount := getUserInput("How much do you want to invest: R")
+	years := getUserInput("Over how many years do you want to invest: ")
+	expectedReturnRate := getUserInput("What is your expected return rate: ")
 
 	// Calculations
-	futureValue := investmentAmount * math.Pow(1+expectedReturnRate/100, years)
-	futureRealValue := futureValue / math.Pow(1+inflationRate/100, years)
+	futureValue, futureRealValue := futureValueCalc(investmentAmount, years, expectedReturnRate)
 
 	// Outputs
-	formattedFV := fmt.Sprintf("Future value of investment: R%.2f\n", futureValue)
-	formattedRFV := fmt.Sprintf("Future value of investment adjusted for inflation: R%.2f\n", futureRealValue)
-
+	formattedFV, formattedRFV := outputs(futureValue, futureRealValue)
 	fmt.Print(formattedFV, formattedRFV)
+}
+
+func getUserInput(textInfo string) float64 {
+	var userInput float64
+	fmt.Print(textInfo)
+	fmt.Scan(&userInput)
+	return userInput
+}
+
+func futureValueCalc(investmentAmount, years, expectedReturnRate float64) (float64, float64) {
+	fv := investmentAmount * math.Pow(1+expectedReturnRate/100, years)
+	rfv := fv / math.Pow(1+inflationRate/100, years)
+	return fv, rfv
+}
+
+func outputs(futureValue, futureRealValue float64) (string, string) {
+	formattedfv := fmt.Sprintf("Future value of investment: R%.2f\n", futureValue)
+	formattedrfv := fmt.Sprintf("Future value of investment adjusted for inflation: R%.2f\n", futureRealValue)
+	return formattedfv, formattedrfv
 }
